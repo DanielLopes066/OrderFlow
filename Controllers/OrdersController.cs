9b0c676c9202c5dd05f85098212f54d279634070
Daniel Lopes <danielmichel133@gmail.com>
2026-04-13 20:48:12 -0300

    Initial commit

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.AccessControl;
+using Microsoft.AspNetCore.Mvc;
+using OrderFlow.Models;
+using OrderFlow.Services;
+
+namespace OrderFlow.Controllers {
+
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase {
+        private OrderService takeOrder;
+
+
+        public OrdersController(OrderService _orderService) {
+            takeOrder = _orderService;
+        }
+
+        [HttpGet()]
+        public IActionResult GetOrders() {
+
+            var showMainOrder = takeOrder.ReturnOrderList();
+
+            return Ok(showMainOrder);
+        }
+    }
+}
+
+
+
---
+++ Services/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using OrderFlow.Models;
+
+namespace OrderFlow.Services {
+
+    public class OrderService {
+
+        public List<Order> ReturnOrderList() {
+
+            var ordersList = new List<Order>() {
+
+                 new Order {
+                     Id = 1,
+                     CustomerName = "Monroe",
+                     TotalAmount = 10m
+                 },
+                 new Order {
+                     Id = 2,
+                     CustomerName = "Stevesson",
+                     TotalAmount = 15m
+                 }, new Order {
+                     Id = 3,
+                     CustomerName = "Thompson",
+                     TotalAmount = 7m
+                 }
+            };
+
+            return ordersList;
+        }
+    }
+}
